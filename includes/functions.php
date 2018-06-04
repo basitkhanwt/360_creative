89a293292bb675701c2c1928fbaa0e871b287124
basitkhanwt <39464338+basitkhanwt@users.noreply.github.com>
2018-06-04 14:12:16 +0500

    php

---
+++ includes/functions.php
@@ -0,0 +1,32 @@
+<?php
+	function hash_generate(max)
+	{
+		
+	}
+	public function Index()//all projects
+	{
+		
+	}
+
+	public function MenuSort(type)//menu selected
+	{
+		
+	}
+
+	public function ProjectOpen(id)//project open
+	{
+
+	}
+
+	public function ProjectCreate(name, logo, poster, description, type)//Project create
+	{
+		
+	}
+
+	public function FunctionName($value='')
+	{
+		# code...
+	}
+
+
+?>
---
+++ project/index.php
@@ -0,0 +1,35 @@
+<?php 
+	
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Project name</title>
+	<link rel="stylesheet" href="../css/reset.css">
+	<link rel="stylesheet" href="../css/ui.css">
+	<link rel="stylesheet" href="../css/grid.css">
+</head>
+<body>
+	<div class="grid">
+		<div class="content">
+			<div class="row">
+				<div class="col-1"><img src="../img/human_young_woman.png"></div>
+				<div class="col-1"><img src="../img/human_young_woman.png"></div>
+				<div class="col-1"><img src="../img/Layer 10.png"></div>
+			</div>
+			<div class="row">
+				<div class="col-1"><img src="../img/shutterstock_514961011.png"></div>
+				<div class="col-2"><img src="../img/Enamel-Mug.png"></div>
+			</div>
+			<div class="row">
+				<div class="col-3"><img src="../img/sauce_bottle_2.png"></div>
+			</div>
+			<div class="row">
+				<div class="col-2"></div>
+				<div class="col-1"></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
